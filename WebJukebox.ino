6974ed249b155dda1a048766db64808be9eafa62
dometdemont <43113699+dometdemont@users.noreply.github.com>
2022-03-28 22:31:42 +0200

    Logo vectoriel

--- WebJukebox.ino
+++ WebJukebox.ino
@@ -175,16 +175,67 @@ table {width: 100%;}
 </head>
 <link rel="icon" href="data:;base64,iVBORw0KGgo=">
 <body><table><tr>
-<td><svg version="1.1"
+<td><svg id='logo' version="1.1"
      baseProfile="full"
      width="30" height="20"
      xmlns="http://www.w3.org/2000/svg">
   <rect width="100%" height="100%" fill="black" />
-  <circle cx="15" cy="10" r="9" fill="green" />
 </svg>
 </td>
 <td><h1>Les grandes orgues</h1><h2>de la Basilique St Joseph de Grenoble</h2></tr></td>
-<table>
+</table>
+<script>
+var xmlns = "http://www.w3.org/2000/svg";
+
+pipe = function(x, y, height, width, mouth){
+	var polygon = document.createElementNS(xmlns, "polygon");
+	var mwidth=height/60
+	polygon.setAttribute("style","fill:black;stroke:green;")
+	var points=(x+width/2).toString()+','+(y-mouth).toString()
+	points+=' '+(x-width/2).toString()+','+(y-mouth).toString()
+	points+=' '+(x-width/2).toString()+','+(y-mouth-height).toString()
+	points+=' '+(x+width/2).toString()+','+(y-mouth-height).toString()
+	points+=' '+(x+width/2).toString()+','+(y-mouth).toString()
+	points+=' '+x.toString()+','+y.toString()
+	points+=' '+(x-width/2).toString()+','+(y-mouth).toString()
+	points+=' '+(x-width/2).toString()+','+(y-mouth-mwidth).toString()
+	points+=' '+(x+width/2).toString()+','+(y-mouth-mwidth).toString()
+	polygon.setAttribute("points",points); 
+	return polygon
+}
+plateface = function(nbPipes, x, y, firstH, lastH, width, firstM, lastM, symetric){
+	var deltaH=(lastH-firstH)/(nbPipes-1)
+	var deltaM=(lastM-firstM)/(nbPipes-1)
+	if(symetric){deltaH*=2; deltaM*=2;}
+	var h=firstH
+	var m=firstM
+	var padding=0
+	for(let p=0; p<nbPipes; p++){
+		if(symetric && p == ~~(nbPipes/2)){
+			deltaH*=-1
+			deltaM*=-1
+		}
+		document.getElementById('logo').appendChild(pipe(x+p*(width+padding), y, h, width, m))
+		h+=deltaH
+		m+=deltaM
+	}
+	return x+width/2+(nbPipes-1)*(width+padding)
+}
+logo = function(x0, y0, scale){
+	var s=scale
+	var nA=3, nB=5, nC=10, nD=9
+	var dy=7*s, p=4.5*s, wA=5*s, wB=3*s, wC=3*s, wD=2*s
+	var x1=plateface(nA, x0, y0-dy, s*77, s*88, wA, s*17, s*17, true)+p
+	var x2=plateface(nB, x1, y0-dy, s*68, s*42, wB, s*13, s*17, false)+p
+	var x3=plateface(nC, x2, y0, s*42, s*30, wC, s*11, s*11, false)+p
+	var x4=plateface(nD, x3, y0, s*32, s*41, wD, s*11, s*6, true)+p
+	var x5=plateface(nC, x4, y0, s*30, s*42, wC, s*11, s*11, false)+p
+	var x6=plateface(nB, x5, y0-dy, s*42, s*68, wB, s*17, s*13, false)+p
+	var x7=plateface(nA, x6, y0-dy, s*77, s*88, wA, s*17, s*17, true)+p
+}
+
+logo(250, 480, 0.7)
+</script>
 )";
 const char * pageFooter = "</body></html>";
 const char *refreshOrCancel = R"(
